0d3cecd4c6eb9f8c6619841322e9cf65d9c7836d
icattlecoder <icattlecoder@gmail.com>
2013-12-18 10:42:53 +0800

    add onProgress event

---
+++ io/UpFile.go
@@ -0,0 +1,47 @@
+package io
+
+import (
+	"io"
+	"os"
+)
+
+type UpFile struct {
+	uploaded   int64
+	file       *os.File
+	tag        bool
+	fsize      int64
+	onProgress func(file_size, uploaded int64)
+}
+
+func OpenUpFile(name string, onProgress func(file_size, uploaded int64)) (pfile *UpFile, err error) {
+	f, err := os.Open(name)
+	pfile = new(UpFile)
+	pfile.file = f
+	fi, err := pfile.file.Stat()
+	if err != nil {
+		return
+	}
+	pfile.onProgress = onProgress
+	pfile.fsize = fi.Size()
+	return
+}
+
+func (pfile *UpFile) Close() {
+	pfile.file.Close()
+}
+
+func (pfile *UpFile) Read(b []byte) (n int, err error) {
+
+	n, err = pfile.file.Read(b)
+	if err == io.EOF {
+		go pfile.onProgress(pfile.fsize, pfile.fsize)
+		return
+	}
+	if !pfile.tag {
+		pfile.tag = true
+		return
+	}
+	go pfile.onProgress(pfile.fsize, pfile.uploaded)
+	pfile.uploaded += int64(n)
+	return
+}
